45ebf8e70d7f81f2354ee1725004550c4435c625
Glitch (nickel-petite-traffic) <none>
2020-12-18 17:53:32 +0000

    🏟🍎 Checkpoint ./config.js:26293121/113 ./server.js:26293121/398

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
-exports.Token = ""; //Bot Token - Important
-exports.Owner = ""; //Bot Owner ID - Important
-exports.Default_Prefix = ""; //Bot Default Prefix (Examples: A!, !)- Important
-exports.Color = ""; //Bot All Embeds Color - Use CAPS For Name (Examples: BLUE, RANDOM) - Important
-exports.Support = ``; //Support Server Link - Never Gonna Give You Up (If No Link Provided)
+exports.Token = "Nzg4ODIyNTI3NzUzMTkxNDY0.X9pGOg._N4kxuQQsahGAW4Qa2fe5UYKaCQ"; //Bot Token - Important
+exports.Owner = "764596916703985734"; //Bot Owner ID - Important
+exports.Default_Prefix = "!"; //Bot Default Prefix (Examples: A!, !)- Important
+exports.Color = "RANDOM"; //Bot All Embeds Color - Use CAPS For Name (Examples: BLUE, RANDOM) - Important
+exports.Support = `https://discord.gg/PBhS95zAEt`; //Support Server Link - Never Gonna Give You Up (If No Link Provided)
 exports.Donate = `https://www.paypal.com/paypalme/legendemoji`; //Please Don't Change It, If You Want To Support Me <3
