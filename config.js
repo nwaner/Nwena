75685fed64ba9a7e5bcad50c1210448c31849d07
Glitch (nickel-petite-traffic) <none>
2020-12-26 18:28:12 +0000

    🍩🐑 Checkpoint ./config.js:26430433/102 ./server.js:26430433/29

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
-exports.Token = "Token-bot"; //Bot Token - Important
-exports.Owner = "id-xow"; //Bot Owner ID - Important
-exports.Default_Prefix = "comandaka"; //Bot Default Prefix (Examples: A!, !)- Important
+exports.Token = "NzkyNDU3MTQyMjc0NjIxNDYw.X-d_OQ.0MKsS9K3p-Zic62RuaVtmPROnr0"; //Bot Token - Important
+exports.Owner = "637299944939585576"; //Bot Owner ID - Important
+exports.Default_Prefix = "+"; //Bot Default Prefix (Examples: A!, !)- Important
 exports.Color = "RANDOM"; //Bot All Embeds Color - Use CAPS For Name (Examples: BLUE, RANDOM) - Important
 exports.Support = `linke server arbel`; //Support Server Link - Never Gonna Give You Up (If No Link Provided)
 exports.Donate = `https://www.paypal.com/paypalme/legendemoji`; //Please Don't Change It, If You Want To Support Me <3
--- server.js
+++ server.js
@@ -9,7 +9,7 @@ client.queue = new Map();
 
 client.on("ready", async () => {
   console.log(`Bot Is Ready To Go!\nTag: ${client.user.tag}`);
-  client.user.setActivity(`!help/setprifex`, { type: "PLAYING" });
+  client.user.setActivity(`+help | Dark Music`, { type: "PLAYING" });
 });
 
 let modules = ["Config", "Music", "Other"];
