f4b159db4a84cf3b0d386497a3cb7a91eed28bc5
Glitch (nickel-petite-traffic) <none>
2020-12-27 16:53:20 +0000

    🐔😎 Checkpoint ./config.js:26715197/77

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-exports.Token = "Nzg4MTYwODA4OTMxOD"; //Bot Token - Important
+exports.Token = "Nzg4MTYwODA4OTMxODE5NTYx.X9fd9A.0rSZ8j9mwWXL7fA_DeWXplHdbWg"; //Bot Token - Important
 exports.Owner = "709790343972782090"; //Bot Owner ID - Important
 exports.Default_Prefix = "?"; //Bot Default Prefix (Examples: A!, !)- Important
 exports.Color = "RANDOM"; //Bot All Embeds Color - Use CAPS For Name (Examples: BLUE, RANDOM) - Important
