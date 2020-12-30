0f91bc84bfa35435515194e2f70b08af85265fc0
Glitch (nickel-petite-traffic) <none>
2020-12-30 20:20:05 +0000

    👶🚓 Checkpoint ./config.js:26715197/1054

--- config.js
+++ config.js
@@ -3,4 +3,4 @@ exports.Owner = "709790343972782090"; //Bot Owner ID - Important
 exports.Default_Prefix = "?"; //Bot Default Prefix (Examples: A!, !)- Important
 exports.Color = "RANDOM"; //Bot All Embeds Color - Use CAPS For Name (Examples: BLUE, RANDOM) - Important
 exports.Support = `https://discord.gg/n9TQs5n`; //Support Server Link - Never Gonna Give You Up (If No Link Provided)
- //Please Don't Change It, If You Want To Support Me <3
+//Please Don't Change It, If You Want To Support Me <3
